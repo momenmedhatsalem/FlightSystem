d1658b5bdc921bd30626207cfce0ee70566b1fe4
momenmedhatsalem <momenmedhat10@hotmail.com>
2024-05-01 18:07:23 +0300

    Started building Booking Page

--- FlightSystem/Form1.Designer.cs
+++ FlightSystem/Form1.Designer.cs
@@ -28,21 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lbl_booking = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
+            // 
+            // lbl_booking
+            // 
+            this.lbl_booking.AutoSize = true;
+            this.lbl_booking.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.lbl_booking.Location = new System.Drawing.Point(30, 28);
+            this.lbl_booking.Name = "lbl_booking";
+            this.lbl_booking.Size = new System.Drawing.Size(172, 31);
+            this.lbl_booking.TabIndex = 0;
+            this.lbl_booking.Text = "Book a Flight";
+            this.lbl_booking.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Alexandria",
+            "Barcelona",
+            "Cairo",
+            "Paris",
+            "Stockholm"});
+            this.comboBox1.Location = new System.Drawing.Point(50, 81);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.Sorted = true;
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.Text = "From";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Alexandria",
+            "Barcelona",
+            "Cairo",
+            "Paris",
+            "Stockholm"});
+            this.comboBox2.Location = new System.Drawing.Point(50, 122);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.Sorted = true;
+            this.comboBox2.TabIndex = 2;
+            this.comboBox2.Text = "To";
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(799, 450);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.lbl_booking);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbl_booking;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
     }
 }
 
--- FlightSystem/Form1.cs
+++ FlightSystem/Form1.cs
@@ -21,5 +21,20 @@ namespace FlightSystem
         {
 
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
