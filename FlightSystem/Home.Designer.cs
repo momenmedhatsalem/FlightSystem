4d76f214647c1e428dc740312d823977ec4e081b
Sayed Reda <sydr38734@gmail.com>
2024-05-08 15:33:51 +0300

    Added Home form with some icons

---
+++ FlightSystem/Home.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace FlightSystem
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Home));
+            this.homeBtn = new System.Windows.Forms.Button();
+            this.flightsBtn = new System.Windows.Forms.Button();
+            this.aircraftsBtn = new System.Windows.Forms.Button();
+            this.bookingBtn = new System.Windows.Forms.Button();
+            this.signoutBtn = new System.Windows.Forms.Button();
+            this.profileBtn = new System.Windows.Forms.Button();
+            this.menuPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.logoPanel = new System.Windows.Forms.Panel();
+            this.logo = new System.Windows.Forms.Panel();
+            this.menuBtn = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.headerLabel = new System.Windows.Forms.Label();
+            this.homePanel = new System.Windows.Forms.Panel();
+            this.menuPanel.SuspendLayout();
+            this.logoPanel.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // homeBtn
+            // 
+            this.homeBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.homeBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.homeBtn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.homeBtn.FlatAppearance.BorderSize = 0;
+            this.homeBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.homeBtn.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.homeBtn.ForeColor = System.Drawing.Color.White;
+            this.homeBtn.Image = ((System.Drawing.Image)(resources.GetObject("homeBtn.Image")));
+            this.homeBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.homeBtn.Location = new System.Drawing.Point(3, 144);
+            this.homeBtn.Name = "homeBtn";
+            this.homeBtn.Padding = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.homeBtn.Size = new System.Drawing.Size(194, 44);
+            this.homeBtn.TabIndex = 1;
+            this.homeBtn.Tag = "Home";
+            this.homeBtn.Text = "Home";
+            this.homeBtn.UseVisualStyleBackColor = true;
+            // 
+            // flightsBtn
+            // 
+            this.flightsBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.flightsBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.flightsBtn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flightsBtn.FlatAppearance.BorderSize = 0;
+            this.flightsBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.flightsBtn.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.flightsBtn.ForeColor = System.Drawing.Color.White;
+            this.flightsBtn.Image = ((System.Drawing.Image)(resources.GetObject("flightsBtn.Image")));
+            this.flightsBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.flightsBtn.Location = new System.Drawing.Point(3, 194);
+            this.flightsBtn.Name = "flightsBtn";
+            this.flightsBtn.Padding = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.flightsBtn.Size = new System.Drawing.Size(194, 44);
+            this.flightsBtn.TabIndex = 2;
+            this.flightsBtn.Tag = "Flights";
+            this.flightsBtn.Text = "Flights";
+            this.flightsBtn.UseVisualStyleBackColor = true;
+            // 
+            // aircraftsBtn
+            // 
+            this.aircraftsBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.aircraftsBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.aircraftsBtn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.aircraftsBtn.FlatAppearance.BorderSize = 0;
+            this.aircraftsBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.aircraftsBtn.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.aircraftsBtn.ForeColor = System.Drawing.Color.White;
+            this.aircraftsBtn.Image = ((System.Drawing.Image)(resources.GetObject("aircraftsBtn.Image")));
+            this.aircraftsBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.aircraftsBtn.Location = new System.Drawing.Point(3, 244);
+            this.aircraftsBtn.Name = "aircraftsBtn";
+            this.aircraftsBtn.Padding = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.aircraftsBtn.Size = new System.Drawing.Size(194, 44);
+            this.aircraftsBtn.TabIndex = 3;
+            this.aircraftsBtn.Tag = "Aircrafts";
+            this.aircraftsBtn.Text = "Aircrafts";
+            this.aircraftsBtn.UseVisualStyleBackColor = true;
+            // 
+            // bookingBtn
+            // 
+            this.bookingBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.bookingBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bookingBtn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.bookingBtn.FlatAppearance.BorderSize = 0;
+            this.bookingBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bookingBtn.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bookingBtn.ForeColor = System.Drawing.Color.White;
+            this.bookingBtn.Image = ((System.Drawing.Image)(resources.GetObject("bookingBtn.Image")));
+            this.bookingBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.bookingBtn.Location = new System.Drawing.Point(3, 294);
+            this.bookingBtn.Name = "bookingBtn";
+            this.bookingBtn.Padding = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.bookingBtn.Size = new System.Drawing.Size(194, 44);
+            this.bookingBtn.TabIndex = 4;
+            this.bookingBtn.Tag = "Booking";
+            this.bookingBtn.Text = "Booking";
+            this.bookingBtn.UseVisualStyleBackColor = true;
+            // 
+            // signoutBtn
+            // 
+            this.signoutBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.signoutBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.signoutBtn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.signoutBtn.FlatAppearance.BorderSize = 0;
+            this.signoutBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.signoutBtn.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.signoutBtn.ForeColor = System.Drawing.Color.White;
+            this.signoutBtn.Image = ((System.Drawing.Image)(resources.GetObject("signoutBtn.Image")));
+            this.signoutBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.signoutBtn.Location = new System.Drawing.Point(3, 485);
+            this.signoutBtn.Name = "signoutBtn";
+            this.signoutBtn.Padding = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.signoutBtn.Size = new System.Drawing.Size(194, 44);
+            this.signoutBtn.TabIndex = 6;
+            this.signoutBtn.Tag = "Exit";
+            this.signoutBtn.Text = "Exit";
+            this.signoutBtn.UseVisualStyleBackColor = true;
+            // 
+            // profileBtn
+            // 
+            this.profileBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.profileBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.profileBtn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.profileBtn.FlatAppearance.BorderSize = 0;
+            this.profileBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.profileBtn.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.profileBtn.ForeColor = System.Drawing.Color.White;
+            this.profileBtn.Image = ((System.Drawing.Image)(resources.GetObject("profileBtn.Image")));
+            this.profileBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.profileBtn.Location = new System.Drawing.Point(3, 435);
+            this.profileBtn.Name = "profileBtn";
+            this.profileBtn.Padding = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.profileBtn.Size = new System.Drawing.Size(194, 44);
+            this.profileBtn.TabIndex = 7;
+            this.profileBtn.Tag = "Profile";
+            this.profileBtn.Text = "Profile";
+            this.profileBtn.UseVisualStyleBackColor = true;
+            // 
+            // menuPanel
+            // 
+            this.menuPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(169)))), ((int)(((byte)(255)))));
+            this.menuPanel.ColumnCount = 1;
+            this.menuPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.menuPanel.Controls.Add(this.logoPanel, 0, 0);
+            this.menuPanel.Controls.Add(this.signoutBtn, 0, 8);
+            this.menuPanel.Controls.Add(this.profileBtn, 0, 7);
+            this.menuPanel.Controls.Add(this.homeBtn, 0, 2);
+            this.menuPanel.Controls.Add(this.bookingBtn, 0, 5);
+            this.menuPanel.Controls.Add(this.flightsBtn, 0, 3);
+            this.menuPanel.Controls.Add(this.aircraftsBtn, 0, 4);
+            this.menuPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.menuPanel.Location = new System.Drawing.Point(0, 0);
+            this.menuPanel.Name = "menuPanel";
+            this.menuPanel.RowCount = 9;
+            this.menuPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.menuPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.menuPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.menuPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.menuPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.menuPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.menuPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.menuPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.menuPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.menuPanel.Size = new System.Drawing.Size(200, 532);
+            this.menuPanel.TabIndex = 3;
+            // 
+            // logoPanel
+            // 
+            this.logoPanel.Controls.Add(this.logo);
+            this.logoPanel.Controls.Add(this.menuBtn);
+            this.logoPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.logoPanel.Location = new System.Drawing.Point(3, 3);
+            this.logoPanel.Name = "logoPanel";
+            this.logoPanel.Size = new System.Drawing.Size(194, 44);
+            this.logoPanel.TabIndex = 0;
+            // 
+            // logo
+            // 
+            this.logo.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("logo.BackgroundImage")));
+            this.logo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.logo.Dock = System.Windows.Forms.DockStyle.Left;
+            this.logo.Location = new System.Drawing.Point(0, 0);
+            this.logo.Name = "logo";
+            this.logo.Size = new System.Drawing.Size(50, 44);
+            this.logo.TabIndex = 0;
+            // 
+            // menuBtn
+            // 
+            this.menuBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.menuBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.menuBtn.Dock = System.Windows.Forms.DockStyle.Right;
+            this.menuBtn.FlatAppearance.BorderSize = 0;
+            this.menuBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.menuBtn.Image = ((System.Drawing.Image)(resources.GetObject("menuBtn.Image")));
+            this.menuBtn.Location = new System.Drawing.Point(124, 0);
+            this.menuBtn.Name = "menuBtn";
+            this.menuBtn.Size = new System.Drawing.Size(70, 44);
+            this.menuBtn.TabIndex = 1;
+            this.menuBtn.UseVisualStyleBackColor = true;
+            this.menuBtn.Click += new System.EventHandler(this.menuBtn_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.headerLabel);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(200, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(678, 50);
+            this.panel1.TabIndex = 4;
+            // 
+            // headerLabel
+            // 
+            this.headerLabel.AutoSize = true;
+            this.headerLabel.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.headerLabel.Location = new System.Drawing.Point(22, 13);
+            this.headerLabel.Name = "headerLabel";
+            this.headerLabel.Size = new System.Drawing.Size(76, 25);
+            this.headerLabel.TabIndex = 0;
+            this.headerLabel.Text = "HOME";
+            this.headerLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // homePanel
+            // 
+            this.homePanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(242)))), ((int)(((byte)(252)))));
+            this.homePanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.homePanel.Enabled = false;
+            this.homePanel.Location = new System.Drawing.Point(200, 50);
+            this.homePanel.Name = "homePanel";
+            this.homePanel.Size = new System.Drawing.Size(678, 482);
+            this.homePanel.TabIndex = 5;
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(878, 532);
+            this.Controls.Add(this.homePanel);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuPanel);
+            this.Name = "Home";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Home";
+            this.menuPanel.ResumeLayout(false);
+            this.logoPanel.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button profileBtn;
+        private System.Windows.Forms.Button signoutBtn;
+        private System.Windows.Forms.Button bookingBtn;
+        private System.Windows.Forms.Button aircraftsBtn;
+        private System.Windows.Forms.Button flightsBtn;
+        private System.Windows.Forms.Button homeBtn;
+        private System.Windows.Forms.TableLayoutPanel menuPanel;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label headerLabel;
+        private System.Windows.Forms.Panel homePanel;
+        private System.Windows.Forms.Panel logoPanel;
+        private System.Windows.Forms.Button menuBtn;
+        private System.Windows.Forms.Panel logo;
+    }
+}
