5b7fb313a98bd9489fcaff1dd3c59edc48f8fcdd
Ziad Mohamad Eisa <118804782+Ziad624@users.noreply.github.com>
2024-05-11 18:28:26 +0300

    Edit Flight Informations has been Finished

--- FlightSystem/EditFlight_Info.Designer.cs
+++ FlightSystem/EditFlight_Info.Designer.cs
@@ -28,6 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.AvailableSeats = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.ArrivalDate = new System.Windows.Forms.DateTimePicker();
             this.DepatureDate = new System.Windows.Forms.DateTimePicker();
@@ -36,131 +39,116 @@
             this.label7 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.AvailableSeats = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // AvailableSeats
+            // 
+            this.AvailableSeats.Location = new System.Drawing.Point(341, 228);
+            this.AvailableSeats.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.AvailableSeats.Name = "AvailableSeats";
+            this.AvailableSeats.Size = new System.Drawing.Size(58, 20);
+            this.AvailableSeats.TabIndex = 40;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(146, 109);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(124, 20);
+            this.label8.TabIndex = 39;
+            this.label8.Text = "Arrival Airport ID";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(146, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 20);
+            this.label1.TabIndex = 38;
+            this.label1.Text = "Departure Airport ID";
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(301, 418);
-            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Location = new System.Drawing.Point(211, 321);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(488, 86);
-            this.button1.TabIndex = 25;
+            this.button1.Size = new System.Drawing.Size(366, 70);
+            this.button1.TabIndex = 37;
             this.button1.Text = "Save Changes";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.Save_Changes_Button);
+            this.button1.Click += new System.EventHandler(this.Update_Data);
             // 
             // ArrivalDate
             // 
-            this.ArrivalDate.Location = new System.Drawing.Point(372, 255);
-            this.ArrivalDate.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.ArrivalDate.Location = new System.Drawing.Point(341, 188);
+            this.ArrivalDate.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.ArrivalDate.Name = "ArrivalDate";
-            this.ArrivalDate.Size = new System.Drawing.Size(200, 22);
-            this.ArrivalDate.TabIndex = 23;
+            this.ArrivalDate.Size = new System.Drawing.Size(151, 20);
+            this.ArrivalDate.TabIndex = 36;
             // 
             // DepatureDate
             // 
-            this.DepatureDate.Location = new System.Drawing.Point(372, 206);
-            this.DepatureDate.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.DepatureDate.Location = new System.Drawing.Point(341, 148);
+            this.DepatureDate.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.DepatureDate.Name = "DepatureDate";
-            this.DepatureDate.Size = new System.Drawing.Size(200, 22);
-            this.DepatureDate.TabIndex = 22;
-            this.DepatureDate.ValueChanged += new System.EventHandler(this.DepatureDate_ValueChanged);
+            this.DepatureDate.Size = new System.Drawing.Size(151, 20);
+            this.DepatureDate.TabIndex = 35;
             // 
             // ArrivalIDs
             // 
             this.ArrivalIDs.FormattingEnabled = true;
-            this.ArrivalIDs.Location = new System.Drawing.Point(396, 156);
-            this.ArrivalIDs.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.ArrivalIDs.Location = new System.Drawing.Point(359, 108);
+            this.ArrivalIDs.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.ArrivalIDs.Name = "ArrivalIDs";
-            this.ArrivalIDs.Size = new System.Drawing.Size(121, 24);
-            this.ArrivalIDs.TabIndex = 21;
-            this.ArrivalIDs.SelectedIndexChanged += new System.EventHandler(this.ArrivalIDs_SelectedIndexChanged);
+            this.ArrivalIDs.Size = new System.Drawing.Size(92, 21);
+            this.ArrivalIDs.TabIndex = 34;
             // 
             // DepatureIDs
             // 
             this.DepatureIDs.FormattingEnabled = true;
-            this.DepatureIDs.Location = new System.Drawing.Point(396, 96);
-            this.DepatureIDs.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.DepatureIDs.Location = new System.Drawing.Point(359, 59);
+            this.DepatureIDs.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.DepatureIDs.Name = "DepatureIDs";
-            this.DepatureIDs.Size = new System.Drawing.Size(121, 24);
-            this.DepatureIDs.TabIndex = 20;
-            this.DepatureIDs.SelectedIndexChanged += new System.EventHandler(this.DispatureAirport);
+            this.DepatureIDs.Size = new System.Drawing.Size(92, 21);
+            this.DepatureIDs.TabIndex = 33;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(112, 255);
-            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Location = new System.Drawing.Point(146, 188);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(113, 25);
-            this.label7.TabIndex = 19;
+            this.label7.Size = new System.Drawing.Size(91, 20);
+            this.label7.TabIndex = 32;
             this.label7.Text = "Arrival Date";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(112, 206);
-            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Location = new System.Drawing.Point(146, 148);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(144, 25);
-            this.label6.TabIndex = 18;
+            this.label6.Size = new System.Drawing.Size(120, 20);
+            this.label6.TabIndex = 31;
             this.label6.Text = "Departure Date";
-            this.label6.Click += new System.EventHandler(this.label6_Click);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(112, 302);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Location = new System.Drawing.Point(146, 226);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(148, 25);
-            this.label5.TabIndex = 17;
+            this.label5.Size = new System.Drawing.Size(118, 20);
+            this.label5.TabIndex = 30;
             this.label5.Text = "Available Seats";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(112, 97);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(184, 25);
-            this.label1.TabIndex = 26;
-            this.label1.Text = "Departure Airport ID";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(112, 157);
-            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(153, 25);
-            this.label8.TabIndex = 28;
-            this.label8.Text = "Arrival Airport ID";
-            // 
-            // AvailableSeats
-            // 
-            this.AvailableSeats.Location = new System.Drawing.Point(372, 304);
-            this.AvailableSeats.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
-            this.AvailableSeats.Name = "AvailableSeats";
-            this.AvailableSeats.Size = new System.Drawing.Size(76, 22);
-            this.AvailableSeats.TabIndex = 29;
-            this.AvailableSeats.TextChanged += new System.EventHandler(this.AvailableSeats_TextChanged);
             // 
             // EditFlight_Info
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.AvailableSeats);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label1);
@@ -172,9 +160,8 @@
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "EditFlight_Info";
-            this.Text = "EditFlight";
+            this.Text = "Form1";
             this.Load += new System.EventHandler(this.EditFlight_Info_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -183,6 +170,9 @@
 
         #endregion
 
+        private System.Windows.Forms.TextBox AvailableSeats;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.DateTimePicker ArrivalDate;
         private System.Windows.Forms.DateTimePicker DepatureDate;
@@ -191,8 +181,5 @@
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.TextBox AvailableSeats;
     }
 }
