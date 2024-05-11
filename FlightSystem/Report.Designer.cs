64fd3e23c0348ca4567332aba104c2b56bfd8585
Sayed Reda <sydr38734@gmail.com>
2024-05-11 22:09:14 +0300

    Finished Report database stuff

--- FlightSystem/Report.Designer.cs
+++ FlightSystem/Report.Designer.cs
@@ -31,25 +31,15 @@
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
-            this.okBtn = new System.Windows.Forms.Button();
             this.mainPanel = new System.Windows.Forms.TableLayoutPanel();
             this.titleLabel = new System.Windows.Forms.Label();
             this.dataGrid = new System.Windows.Forms.DataGridView();
             this.nameCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.infoCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.okBtn = new System.Windows.Forms.Button();
             this.mainPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
             this.SuspendLayout();
-            // 
-            // okBtn
-            // 
-            this.okBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.okBtn.Location = new System.Drawing.Point(399, 252);
-            this.okBtn.Name = "okBtn";
-            this.okBtn.Size = new System.Drawing.Size(75, 24);
-            this.okBtn.TabIndex = 1;
-            this.okBtn.Text = "OK";
-            this.okBtn.UseVisualStyleBackColor = true;
             // 
             // mainPanel
             // 
@@ -67,7 +57,7 @@
             this.mainPanel.RowCount = 2;
             this.mainPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 60F));
             this.mainPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.mainPanel.Size = new System.Drawing.Size(462, 234);
+            this.mainPanel.Size = new System.Drawing.Size(452, 236);
             this.mainPanel.TabIndex = 2;
             // 
             // titleLabel
@@ -75,7 +65,7 @@
             this.titleLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.titleLabel.AutoSize = true;
             this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.titleLabel.Location = new System.Drawing.Point(151, 17);
+            this.titleLabel.Location = new System.Drawing.Point(146, 17);
             this.titleLabel.Name = "titleLabel";
             this.titleLabel.Size = new System.Drawing.Size(159, 25);
             this.titleLabel.TabIndex = 0;
@@ -99,10 +89,11 @@
             this.nameCol,
             this.infoCol});
             this.dataGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGrid.GridColor = System.Drawing.Color.Silver;
             this.dataGrid.Location = new System.Drawing.Point(3, 63);
             this.dataGrid.Name = "dataGrid";
             this.dataGrid.RowHeadersVisible = false;
-            this.dataGrid.Size = new System.Drawing.Size(456, 168);
+            this.dataGrid.Size = new System.Drawing.Size(446, 170);
             this.dataGrid.TabIndex = 1;
             // 
             // nameCol
@@ -110,8 +101,9 @@
             dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(169)))), ((int)(((byte)(255)))));
             dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.White;
             this.nameCol.DefaultCellStyle = dataGridViewCellStyle2;
-            this.nameCol.HeaderText = "Name";
+            this.nameCol.HeaderText = "Title";
             this.nameCol.Name = "nameCol";
+            this.nameCol.Width = 120;
             // 
             // infoCol
             // 
@@ -121,15 +113,32 @@
             this.infoCol.HeaderText = "Info";
             this.infoCol.Name = "infoCol";
             // 
+            // okBtn
+            // 
+            this.okBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.okBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(169)))), ((int)(((byte)(255)))));
+            this.okBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.okBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.okBtn.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.okBtn.ForeColor = System.Drawing.Color.White;
+            this.okBtn.Location = new System.Drawing.Point(384, 254);
+            this.okBtn.Name = "okBtn";
+            this.okBtn.Size = new System.Drawing.Size(80, 30);
+            this.okBtn.TabIndex = 10;
+            this.okBtn.Text = "OK";
+            this.okBtn.UseVisualStyleBackColor = false;
+            this.okBtn.Click += new System.EventHandler(this.okBtn_Click);
+            // 
             // Report
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(242)))), ((int)(((byte)(252)))));
-            this.ClientSize = new System.Drawing.Size(486, 288);
-            this.Controls.Add(this.mainPanel);
+            this.ClientSize = new System.Drawing.Size(476, 290);
             this.Controls.Add(this.okBtn);
+            this.Controls.Add(this.mainPanel);
             this.Name = "Report";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.mainPanel.ResumeLayout(false);
             this.mainPanel.PerformLayout();
@@ -139,11 +148,10 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button okBtn;
         private System.Windows.Forms.TableLayoutPanel mainPanel;
         private System.Windows.Forms.Label titleLabel;
         private System.Windows.Forms.DataGridView dataGrid;
+        private System.Windows.Forms.Button okBtn;
         private System.Windows.Forms.DataGridViewTextBoxColumn nameCol;
         private System.Windows.Forms.DataGridViewTextBoxColumn infoCol;
     }
--- FlightSystem/Report.cs
+++ FlightSystem/Report.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static FlightSystem.Program;
 
 namespace FlightSystem
 {
@@ -27,17 +29,81 @@ namespace FlightSystem
             this.dataGrid.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
 
             // Rows
-            this.dataGrid.Rows[0].Cells[0].Value = "Most visited citeis";
-            this.dataGrid.Rows[1].Cells[0].Value = "Most used aircrafts";
-            this.dataGrid.Rows[2].Cells[0].Value = "Most used airports";
+            this.dataGrid.Rows[0].Cells[0].Value = "Most frequent aircrafts";
+            this.dataGrid.Rows[1].Cells[0].Value = "Most frequent airport";
+            this.dataGrid.Rows[2].Cells[0].Value = "Most visited citeis";
+
+            // Data
+            pushData();
             
         }
 
+        // Don't forget to close reader
         private void pushData()
         {
-            // Fetch most visited cities and aircrafts and airports and push them
+            // Queries
+            string[] queries = new string[3];
+            queries[0] = @"
+                select top 1 AIRCRAFTNAME
+                from dbo.AIRCRAFT left outer join SCHEMA_1.FLIGHT
+                on FLIGHT.AIR_AIRCRAFTID = AIRCRAFT.AIRCRAFTID
+                group by AIRCRAFTID, AIRCRAFTNAME
+                order by count(flightid) desc;
+             ";
+            queries[1] = @"
+                select AIRPORT.AIRPORTNAME
+                from AIRPORT
+                where AIRPORTID = (
+	                select top 1 ID
+	                from (
+			                select DEPARTURE_AIRPORTID2 as ID, count(PAS_PASSENGERID) as cnt
+			                from dbo.BOARDING b inner join SCHEMA_1.FLIGHT f on b.FLI_FLIGHTID = f.FLIGHTID
+			                group by DEPARTURE_AIRPORTID2
+			                union
+			                select ARRIVAL_AIRPORTID2 as ID, count(PAS_PASSENGERID) as cnt
+			                from dbo.BOARDING b inner join SCHEMA_1.FLIGHT f on b.FLI_FLIGHTID = f.FLIGHTID
+			                group by ARRIVAL_AIRPORTID2
+	                ) as result
+	                group by ID
+	                order by sum(cnt) desc
+                );
+            ";
+            queries[2] = @"
+                select AIRPORT.CITY from AIRPORT
+                where AIRPORT.AIRPORTID = (
+	                select top 1 ARRIVAL_AIRPORTID2
+	                from SCHEMA_1.FLIGHT as f inner join dbo.BOARDING as r on f.FLIGHTID = r.FLI_FLIGHTID
+	                group by f.ARRIVAL_AIRPORTID2
+	                order by count(PAS_PASSENGERID) desc
+                );
+             ";
+            using (SqlConnection connection = new SqlConnection(AppGlobals.connString))
+            {
+                connection.Open();
+                for (int i = 0; i < 3; ++i)
+                {
+                    // Create SqlCommand with query and connection
+                    using (SqlCommand command = new SqlCommand(queries[i], connection))
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            reader.Read();
+                            this.dataGrid.Rows[i].Cells[1].Value = reader.GetString(0);
+                        }
+                        else
+                        {
+                            this.dataGrid.Rows[i].Cells[1].Value = "No records found";
+                        }
+                    }
+                }
+            }
+        }
 
+        private void okBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
-
+        
 }
